d91d2ce5babe533c6cffcfdf2b6d87636a13963d
Ruth Fong <ruthcfong@gmail.com>
2019-11-11 21:50:52 +0000

    wip: add weighting strategy and parameterize script

--- torchray/attribution/weighted_saliency.py
+++ torchray/attribution/weighted_saliency.py
@@ -11,11 +11,22 @@ def normalize(x):
 def weighted_saliency(saliency_func,
                       layer_weights,
                       *args,
+                      weights_strategy="activation",
                       accumulation="sum",
                       **kwargs):
     if not isinstance(layer_weights, dict):
         raise TypeError(f"layer_weights not a dict; it's a {type(layer_weights)}")
-
+    if weights_strategy in ["activation", "accuracy"]:
+        pass
+    elif weights_strategy == "uniform":
+        layer_weights = {layer_name: 1. for layer_name in layer_weights.keys()}
+    elif weights_strategy == "linear":
+        weights = np.linspace(0, 1, len(layer_weights))[1:]
+        layer_weights = {layer_name: weights[i] for i, layer_name in enumerate(layer_weights.keys())}
+    else:
+        raise NotImplementedError(f"weights_strategy should be ['activation'"
+                                  f", 'accuracy', 'uniform', 'linear']; "
+                                  f"instead, it's {weights_strategy}.")
     norm_term = np.sum(list(layer_weights.values()))
 
     cum_saliency_map = 0
